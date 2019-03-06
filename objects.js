6884c6af6afea054754e9fd961b89c5f6939fe27
Gino Desangles <blushingbox@yahoo.com>
2019-03-06 01:42:18 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,8 @@
 var playlist = {}
 
-function updatePlaylist(object, name, title) {
+function updatePlaylist(object, title) {
   object[name] = title
   return object
 }
+
+function removeFromPlaylist(playlist, artistName)
